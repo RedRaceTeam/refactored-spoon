4bcc384e926b918e61593411dffe6d7f58a4ab3e
RedRaceTeam <katrinkuzua12@gmail.com>
2026-08-14 04:14:30 +0700

    Update config.py

--- core/config.py
+++ core/config.py
@@ -3,8 +3,7 @@ import os
 class Settings:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-    WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-    WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "default_secret")
+    WEBHOOK_URL = "https://refactored-spoon-c3i8.onrender.com"  # публичная ссылка
     PORT = int(os.getenv("PORT", 8000))
     ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 
